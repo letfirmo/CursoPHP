f3b3d021228c8d7462dfeb579df009b6a7d7d6a7
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2024-05-16 17:02:31 -0300

    final da seção 3

---
+++ 3_tipos_de_dados/14_null/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    echo NULL;
+
+    $nome = NULL;
+
+    if(is_null($nome)) {
+        echo "O valor é nulo";
+    }
+
+    $nomr = "Matheus";
+
+    echo "<br>";
+    
+    if(is_null($nome)) {
+        echo "O valor é nulo";
+    }
