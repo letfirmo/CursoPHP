71509e4a5426236aaf0cd589f2810518d40c10cd
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2024-05-16 13:11:56 -0300

    strings

---
+++ 3_tipos_de_dados/5_strings/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    echo "Testando texto de aspas duplas <br>";
+    echo 'Testando texto de aspas simples <br>';
+    echo "Ele disse: 'Olá!' <br>";
+    echo 'Ele disse: "Olá!" <br>'; //não pode usar os mesmos tipos de aspas
+
+    $idade = 15;
+
+    echo "ele tem $idade anos <br>";
+    echo 'ele tem $idade anos'; 
