9d21311fc9731f17bd30a1ae90fa9c50e1444a32
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2024-05-16 17:21:27 -0300

    inicio da seção 4

---
+++ 4_variáveis/1_criacao_de_var/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    $teste = "Algum texto";
+
+    echo $teste;
+
+    $num = 10;
+
+    echo "<br>";
+    echo $num;
+
+    $_nome = "leticia";
+
+    echo "<br>";
+    echo $_nome;
+
+
+    $velocidadeMaxima = 300; //segunda palavra com a primeira letra maiuscula
