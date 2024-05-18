aac22221d46046d1d943746e02349b38b90a9f03
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2024-05-17 21:23:32 -0300

    inicio seção 5

---
+++ 5_operadores_expres/1_ordem_operadores/index.php
@@ -0,0 +1,23 @@
+<?php
+
+    $a = 4;
+    $b = 6;
+    $c = 2;
+
+    echo $a + $b * $c;
+    
+    echo "<br>";
+
+    echo ($a + $b) * $c;
+    
+    echo "<br>";
+
+    echo $a + $b / $c;
+
+    echo "<br>";
+
+    echo ($a + $b) / $c;
+
+    echo "<br>";
+
+    //como na matemática, há hierarquia nas operações.
---
+++ 5_operadores_expres/2_mudanca_tipo_implicito/index.php
@@ -0,0 +1,8 @@
+<?php
+
+    $nome = "Leticia";
+    $sobrenome = "Silva";
+
+    $nomeCompleto = $nome . " " . $sobrenome; //concatenando o nome e sobrenome com um espaço.
+    echo $nomeCompleto;
+    echo "<br>";
