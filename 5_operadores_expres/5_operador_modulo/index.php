9b7b26ffec5433b7f3371941cf43b87aa1e4661a
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2024-05-20 20:03:49 -0300

    op aritimeticos, modulos e exponencial

---
+++ 5_operadores_expres/5_operador_modulo/index.php
@@ -0,0 +1,8 @@
+<?php
+
+    echo 12 % 2 . " com módulo <br>"; //o resto da divisão
+
+    echo 12 / 2 . " com divisão <br>";
+
+    echo 15 % 2 . "<br>";
+    echo 13 % 5 . "<br>";
---
+++ 5_operadores_expres/6_exponenciacao/index.php
@@ -0,0 +1,5 @@
+<?php
+
+    echo 5 ** 2 . "<br>"; //5 elevado ao quadrado
+
+    
