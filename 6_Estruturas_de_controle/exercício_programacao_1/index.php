b09c9f45b9f4d079f6ee19b8eb8f8da829298e46
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2024-05-24 00:12:31 -0300

    correção exercicios de programação

--- 6_Estruturas_de_controle/exercício_programacao_1/index.php
+++ 6_Estruturas_de_controle/exercício_programacao_1/index.php
@@ -2,12 +2,12 @@
 
 function compararNumeros($a,$b){
     if($a > $b){
-        echo "O primeiro número é maior.";
+        return "O primeiro número é maior.";
     } else if($a < $b) {
-        echo "O segundo número é maior.";
+        return "O segundo número é maior.";
     } else {
-        echo "Os números são iguais.";
+        return "Os números são iguais.";
     }
 }
 
-compararNumeros(5,6);
+echo compararNumeros(10,10);
