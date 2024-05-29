695baec04edb5f19ab901166a6e275896c1c3c7e
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2024-05-28 21:54:58 -0300

    inicio seção 9

---
+++ 9_funcoes/2_chamando_funcoes/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    // definindo funcao
+    function soma() {
+
+        $a = 5;
+        $b = 7;
+
+        echo $a + $b . "<br>";
+
+    }
+
+    //chamndo / invoncanado cuncao
+    soma();
+
+    //funcao do php que deixa as letras maiusculas
+    echo strtoupper("testando função");
