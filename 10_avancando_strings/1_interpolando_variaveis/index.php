000a9f1acd2a656f67dcd94be9b2d957ac23160d
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2024-06-06 21:05:38 -0300

    inicio seção 10

---
+++ 10_avancando_strings/1_interpolando_variaveis/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    //o ideal é se manter apenas com uma das sintaxes
+
+    $nome = "Matheus";
+    $idade = 29;
+    $profissao = "Programador";
+
+    echo "Eu sou o $nome e tenho $idade anos, e atuo como $profissao <br>";
+
+    echo "Eu sou o {$nome} e tenho {$idade} anos, e atuo como {$profissao} <br>";
