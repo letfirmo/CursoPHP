0cf29bdda58165b415e0e883d11089cdb78d835c
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2024-07-16 22:29:11 -0300

    continuar fonts

---
+++ 15_BLOG/index.php
@@ -0,0 +1,18 @@
+<?php
+    include_once("helpers/url.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Codar</title>
+    <!-- ESTILOS DO PROJETO -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
+<!-- GOOGLE FONTS -->
+
+</head>
+<body>
+    <h1>Olá mundo!</h1>
+</body>
+</html>
