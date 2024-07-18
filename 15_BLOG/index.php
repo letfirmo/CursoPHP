5f5457ce7b06021054fe574d9aff69eb3fbeb0c7
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2024-07-18 15:24:24 -0300

    change

--- 15_BLOG/index.php
+++ 15_BLOG/index.php
@@ -10,6 +10,8 @@
     <!-- ESTILOS DO PROJETO -->
     <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
 <!-- GOOGLE FONTS -->
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
 
 </head>
 <body>
