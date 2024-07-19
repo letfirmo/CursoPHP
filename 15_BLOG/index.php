3083b36a1977be3fb0e4665bf6da45effc934aa7
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2024-07-18 21:19:46 -0300

    bug

--- 15_BLOG/index.php
+++ 15_BLOG/index.php
@@ -1,7 +1,23 @@
 <?php
     include_once("templates/header.php");
+    print_r($posts);
 ?> 
-    <h1>Olá mundo!</h1>
+    <main>
+        <div id="title-container">
+            <h1>Blog Codar</h1>
+            <p>O seu blog de programação</p>
+        </div>
+        <div id="posts-container">
+            <?php foreach($posts as $post): ?>
+                <div class="post-box">
+                    <img src="<?= $BASE_URL ?>/img/<?= $post['img'] ?>" alt="<?= $post['title'] ?> ">
+                    <h2 class="post-title">
+                        <a href="<?= $BASE_URL ?>post.php/id=<?= $post['id'] ?>"><?= $post['title'] ?></a>
+                    </h2>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </main>
 <?php
     include_once("templates/footer.php");
 ?> 
