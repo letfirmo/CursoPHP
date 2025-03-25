49281f8ef067ffab328d92b2fde1891435945d64
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2025-03-25 14:57:45 -0300

    finalizando validações de autenticação

--- 18_MovieStar/auth_process.php
+++ 18_MovieStar/auth_process.php
@@ -8,6 +8,8 @@
 
     $message = new Message($BASE_URL);
 
+    $userDAO = new UserDAO($conn, $BASE_URL);
+
     //Resgata o tipo do formulário
     $type = filter_input(INPUT_POST, "type");
 
@@ -22,8 +24,25 @@
         //Verificação de dados minimos
         if($name && $lastname && $email && $password){
 
+            //Verificar se as senhas batem
+            if($password === $confirmpassword) {
+
+                //verificar se o email já esta cadastrado no sistema
+                if($userDAO->findByEmail($email) == false){
+
+                    echo "Nenhum usuário foi encontrado";
+
+                } else{
+                    //Enviar uma mensagem de erro, usuário já cadastrado
+                    $message->setMessage("E-mail já cadastrado, faça o login", "error", "back");
+                }
+            } else{
+                //Enviar uma mensagem de erro, de dados faltantes
+                $message->setMessage("As senhas não são iguais", "error", "back");
+            }
+
         }else {
-            //Enviar uma mensagem de erro, de dados faltantes
+            //Enviar uma mensagem de erro, senhas nao batem
             $message->setMessage("Por favor, preencha todos os campos.", "error", "back");
         }
     } else if($type === "login"){
--- 18_MovieStar/dao/UserDAO.php
+++ 18_MovieStar/dao/UserDAO.php
@@ -7,7 +7,7 @@
         private $conn;
         private $url;
 
-        public function __construct(PDA $conn, $url){
+        public function __construct(PDO $conn, $url){
             $this->conn = $conn;
             $this->url = $url; 
         }
@@ -41,9 +41,38 @@
         public function authenticateUser($email, $password){
 
         }
-        public function findByEmail($email){
 
+        public function findByEmail($email){
+            
+            if($email != "") {
+                
+                $stmt = $this->conn->prepare("SELECT * FROM users WHERE email = :email");
+
+                $stmt->bindParam(":email", $email);
+                
+                $stmt->execute();
+
+                if($stmt->rowCount() > 0) {
+                    
+                    $data = $stmt->fetch();
+                    $user = $this->buildUser($data);
+
+                    return $user;
+
+                } else {
+        
+                    return false;
+        
+                }
+        
+            } else{
+        
+                return false;
+        
+            }
+        
         }
+        
         public function findById($id){
 
         }
